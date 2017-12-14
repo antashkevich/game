4a8aa12ca06b3131fa4f389de932ef0d715d7d23
Alexandr Antashkevich <antoha_ale6@mail.ru>
2017-12-14 22:30:21 +0300

    first commit

---
+++ build/js/index.js
@@ -0,0 +1,28 @@
+webpackJsonp([1],[
+/* 0 */,
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__index_scss__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__index_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__index_scss__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_normalize_css__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_normalize_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_normalize_css__);
+	
+
+
+
+/*import createMenu from '../../components/menu/menu';
+var menu = createMenu(['Главная','Блог'], 'menu');
+document.body.appendChild(menu);*/
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+],[2]);
